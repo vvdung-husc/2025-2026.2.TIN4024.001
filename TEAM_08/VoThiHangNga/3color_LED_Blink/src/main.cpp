fbf8fa1bb45134d5c3c0239513c6271154e154a3
hanga17 <vothihangnga352004@gmail.com>
2026-01-26 22:16:18 +0700

    update 3color_LED_Blink

--- TEAM_08/VoThiHangNga/3color_LED_Blink/src/main.cpp
+++ TEAM_08/VoThiHangNga/3color_LED_Blink/src/main.cpp
@@ -1,35 +1,41 @@
 #include <Arduino.h>
-#define LED_RED    25
-#define LED_YELLOW 33
-#define LED_GREEN  32
+
+#define LED_RED     25
+#define LED_YELLOW  33
+#define LED_GREEN   32
+
+// Hàm cho 1 đèn nháy trong X giây
+void blinkLED(int ledPin, int totalTimeMs) {
+  int blinkDelay = 500; // 0.5s sáng, 0.5s tắt
+  int elapsed = 0;
+
+  while (elapsed < totalTimeMs) {
+    digitalWrite(ledPin, HIGH);
+    delay(blinkDelay);
+    digitalWrite(ledPin, LOW);
+    delay(blinkDelay);
+    elapsed += blinkDelay * 2;
+  }
+}
 
 void setup() {
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_YELLOW, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
 
-  // Tắt hết đèn lúc bắt đầu
+  // Tắt hết lúc bắt đầu
   digitalWrite(LED_RED, LOW);
   digitalWrite(LED_YELLOW, LOW);
   digitalWrite(LED_GREEN, LOW);
 }
 
 void loop() {
-  // 🔴 Đèn đỏ sáng 10s
-  digitalWrite(LED_RED, HIGH);
-  digitalWrite(LED_YELLOW, LOW);
-  digitalWrite(LED_GREEN, LOW);
-  delay(10000);
+  // 🔴 Đèn đỏ nháy 10s
+  blinkLED(LED_RED, 10000);
 
-  // 🟡 Đèn vàng sáng 10s
-  digitalWrite(LED_RED, LOW);
-  digitalWrite(LED_YELLOW, HIGH);
-  digitalWrite(LED_GREEN, LOW);
-  delay(10000);
+  // 🟡 Đèn vàng nháy 10s
+  blinkLED(LED_YELLOW, 10000);
 
-  // 🟢 Đèn xanh sáng 10s
-  digitalWrite(LED_RED, LOW);
-  digitalWrite(LED_YELLOW, LOW);
-  digitalWrite(LED_GREEN, HIGH);
-  delay(10000);
+  // 🟢 Đèn xanh nháy 10s
+  blinkLED(LED_GREEN, 10000);
 }
