967d0b219fe01f557c301b60b21c14f019434739
lekhanhly-20 <130689096+lekhanhly-20@users.noreply.github.com>
2026-01-30 12:53:34 +0700

    capnhat

--- TEAM_10/LeThiKhanhLy/TRAFFIC_LIGHT/src/main.h
+++ TEAM_10/LeThiKhanhLy/TRAFFIC_LIGHT/src/main.h
@@ -1,24 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
+#pragma once
 
-#include <Arduino.h>
-#include <TM1637Display.h>
-
-// ====== LED GIAO THÔNG ======
-#define LED_RED     27
-#define LED_YELLOW  26
-#define LED_GREEN   25
-
-// ====== LED & BUTTON BLUE ======
-#define LED_BLUE    21
-#define BUTTON_BLUE 23
-
-// ====== TM1637 ======
-#define CLK 18
-#define DIO 19
-
-extern TM1637Display display;
-extern bool blueEnabled;
-extern bool lastButton;
-
-#endif
+#include "ultils.h"
