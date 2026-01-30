4be86c4e7ffbc13223aa94d8e651e40634f8618a
lekhanhly-20 <130689096+lekhanhly-20@users.noreply.github.com>
2026-01-30 12:18:05 +0700

    up

--- TEAM_10/LeThiKhanhLy/TRAFFIC_LIGHT/src/main.cpp
+++ TEAM_10/LeThiKhanhLy/TRAFFIC_LIGHT/src/main.cpp
@@ -1,86 +1,28 @@
-#include <Arduino.h>
-#include <TM1637Display.h>
+#include "main.h"
+#include "utils.h"
 
-// ====== LED GIAO THÔNG ======
-#define LED_RED     27
-#define LED_YELLOW  26
-#define LED_GREEN   25
-
-// ====== LED & BUTTON XANH DƯƠNG ======
-#define LED_BLUE    21
-#define BUTTON_BLUE 23
-
-// ====== TM1637 ======
-#define CLK 18
-#define DIO 19
 TM1637Display display(CLK, DIO);
 
-// ====== BIẾN TRẠNG THÁI ======
-bool countdownEnabled = true;
+// ====== BIẾN TOÀN CỤC ======
+bool blueEnabled = true;
 bool lastButton = HIGH;
 
-// ====== XỬ LÝ NÚT (ĐƠN GIẢN – CHẮC CHẮN ĂN) ======
-void handleButton() {
-  bool current = digitalRead(BUTTON_BLUE);
-
-  // phát hiện cạnh nhấn
-  if (lastButton == HIGH && current == LOW) {
-    countdownEnabled = !countdownEnabled;
-
-    digitalWrite(LED_BLUE, countdownEnabled ? HIGH : LOW);
-
-    if (!countdownEnabled) {
-      display.clear();
-    }
-
-    delay(200); // debounce đơn giản
-  }
-
-  lastButton = current;
-}
-
-// ====== NHẤP NHÁY + ĐẾM NGƯỢC ======
-void blinkWithCountdown(int ledPin, int seconds) {
-  for (int i = seconds; i >= 0; i--) {
-
-    if (countdownEnabled) {
-      display.showNumberDec(i, true);
-    }
-
-    digitalWrite(ledPin, HIGH);
-    for (int t = 0; t < 50; t++) {
-      handleButton();
-      delay(10);
-    }
-
-    digitalWrite(ledPin, LOW);
-    for (int t = 0; t < 50; t++) {
-      handleButton();
-      delay(10);
-    }
-  }
-}
-
 void setup() {
-  Serial.begin(115200);
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_YELLOW, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
 
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_YELLOW, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
+    pinMode(BUTTON_BLUE, INPUT_PULLUP);
 
-  pinMode(BUTTON_BLUE, INPUT_PULLUP);
+    display.setBrightness(7);
+    display.clear();
 
-  display.setBrightness(7);
-  display.clear();
-
-  digitalWrite(LED_BLUE, HIGH);
-
-  Serial.println("SYSTEM READY");
+    digitalWrite(LED_BLUE, HIGH); // mặc định bật
 }
 
 void loop() {
-  blinkWithCountdown(LED_RED, 5);
-  blinkWithCountdown(LED_YELLOW, 3);
-  blinkWithCountdown(LED_GREEN, 7);
+    blinkWithCountdown(LED_RED, 5);
+    blinkWithCountdown(LED_YELLOW, 3);
+    blinkWithCountdown(LED_GREEN, 7);
 }
---
+++ TEAM_10/LeThiKhanhLy/TRAFFIC_LIGHT/src/utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <Arduino.h>
+
+void handleBlueButton();
+void blinkWithCountdown(int ledPin, int seconds);
+
+#endif
