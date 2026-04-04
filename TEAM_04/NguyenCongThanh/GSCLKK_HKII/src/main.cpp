cc703c7f0b32dd069d9419ed23f5994d720d5d02
CongThanh746 <22T1020746@husc.edu.vn>
2026-04-04 23:51:57 +0700

    rebuild

--- TEAM_04/NguyenCongThanh/GSCLKK_HKII/src/main.cpp
+++ TEAM_04/NguyenCongThanh/GSCLKK_HKII/src/main.cpp
@@ -4,9 +4,9 @@
  * Fix: Telegram nhanh hơn + Serial Terminal hiển thị đúng trên Wokwi/VS Code
  */
 
-#define BLYNK_TEMPLATE_ID "TMPL_ID_CUA_BAN"       
-#define BLYNK_TEMPLATE_NAME "Air Quality Monitor" 
-#define BLYNK_AUTH_TOKEN "TOKEN_BLYNK_CUA_BAN"    
+#define BLYNK_TEMPLATE_ID "TMPL6nn2gjF7c"       
+#define BLYNK_TEMPLATE_NAME "AIR QUALITY" 
+#define BLYNK_AUTH_TOKEN "m_JnU9yh0VsAMW8u51VtEfWeBD2cbSx_"    
 
 #include <WiFi.h>
 #include <WiFiClient.h>
